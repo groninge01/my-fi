61d07478d0c8dc918218dfdf60aee2e69d6ec96b
groninge01 <groninge@hotmail.com>
2019-09-30 20:41:59 +0200

    rename my-fi2 > my-fi

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('my-fi2 app is running!');
+    expect(page.getTitleText()).toEqual('my-fi app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-fi2'`, () => {
+  it(`should have as title 'my-fi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-fi2');
+    expect(app.title).toEqual('my-fi');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-fi2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('my-fi app is running!');
   });
 });
